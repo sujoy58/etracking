655e8b5eff172319a5b1e044f3d0c7f77e5c158f
subhajithooghly <subhajitghoshkhanpur@gmail.com>
2022-06-21 11:13:09 +0530

    change h1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Hi there! mettle tech project</h1>
+    <h1>Hlw there! mettle tech project</h1>
     </div>
   );
 }
